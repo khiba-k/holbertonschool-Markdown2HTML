e539f58d459e98cf12d669d60b79524f0bd6935f
khiba-k <khibakoenane@gmail.com>
2024-12-10 13:04:46 +0200

    task1

--- markdown2html.py
+++ markdown2html.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Script to convert markdown to html.
 """
@@ -19,16 +20,15 @@ def main():
     Returns:
         None: The function prints the status to the console and exits on error.
     """
-    
+
     if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py README.md README.html")
+        print("Usage: ./markdown2html.py README.md README.html" , file=sys.stderr)
         sys.exit(1)
 
     file_path = Path(sys.argv[1])
     if not file_path.exists():
-        print(f"Missing {sys.argv[1]}")
+        print(f"Missing {sys.argv[1]}", file=sys.stderr)
         sys.exit(1)
 
 if __name__ == "__main__":
     main()
-
